9dd9b32c41128e8e1abe0f693d3d1e78d5f0c66e
Laura Troost <l_troo01@uni-muenster.de>
2018-06-14 12:52:22 +0200

    spring-boot project

---
+++ loan-approval-spring-boot/src/main/java/org/camunda/bpm/getstarted/loanapproval/SaveInfosDelegate.java
@@ -0,0 +1,14 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SaveInfosDelegate implements JavaDelegate {
+
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
