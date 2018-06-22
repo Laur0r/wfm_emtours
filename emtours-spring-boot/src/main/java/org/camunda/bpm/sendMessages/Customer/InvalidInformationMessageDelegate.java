cd2b6cac66ecf59ced680714a80c67bcfef6853a
cfu01 <c.fukas@gmx.de>
2018-06-22 22:36:57 +0200

    fix sending of mails

--- emtours-spring-boot/src/main/java/org/camunda/bpm/sendMessages/Customer/InvalidInformationMessageDelegate.java
+++ emtours-spring-boot/src/main/java/org/camunda/bpm/sendMessages/Customer/InvalidInformationMessageDelegate.java
@@ -2,12 +2,26 @@ package org.camunda.bpm.sendMessages.Customer;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.mail.EMailerService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InvalidInformationMessageDelegate implements JavaDelegate {
 
+	private EMailerService eMailerService;
+	
+	public InvalidInformationMessageDelegate(EMailerService eMailerService) {
+		this.eMailerService = eMailerService;
+	}
+	
 	public void execute(DelegateExecution execution) throws Exception {
 
-	// TODO Auto-generated method stub
+		try {
+			eMailerService.sendSimpleMessage("redhair@byom.de","Test","Dies ist ein Test");
+			System.out.println("E-Mail sent");
+		}catch (Exception ex) {
+			System.out.println("Error in sending email: "+ex);
+		}
 	
 	}
 }
