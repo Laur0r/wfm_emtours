4ca86b351944bf36fe0d1faca37686f6078d24c3
Laura Troost <l_troo01@uni-muenster.de>
2018-06-22 18:24:41 +0200

    simple send message

--- emtours-spring-boot/src/main/java/org/camunda/bpm/emtours/CustomerController.java
+++ emtours-spring-boot/src/main/java/org/camunda/bpm/emtours/CustomerController.java
@@ -1,12 +1,28 @@
 package org.camunda.bpm.emtours;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.variable.Variables;
 import org.camunda.bpm.engine.variable.value.ObjectValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class CustomerController {
@@ -22,4 +38,35 @@ public class CustomerController {
 		          .putValue("name", name)
 		          .putValue("email", email));
   }
+  
+  @RequestMapping(value="/testSend", method=RequestMethod.POST)
+  public ResponseEntity<String> testPOST(@RequestBody String cust) {
+	  System.out.println("received Post request");
+	  if(cust != null) {
+		  System.out.println("Name: "+cust);
+	  }
+	  return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+  
+ /* private String doPOST(String[] array, String name) {
+	  MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+      Map map = new HashMap<String, String>();
+      map.put("Content-Type", "application/json");
+
+      headers.setAll(map);
+
+      Map req_payload = new HashMap();
+      req_payload.put("name", name);
+
+      HttpEntity<?> request = new HttpEntity<>(req_payload, headers);
+      String url = "http://localhost:8080/testSend";
+
+      String result = "";
+      ResponseEntity<?> response = new RestTemplate().postForEntity(url, request, String.class);
+      HttpStatus statusCode = response.getStatusCode();
+      if (statusCode == HttpStatus.ACCEPTED) {
+          result = (String) response.getBody();
+      }
+      return result;
+	}*/
 }
--- emtours-spring-boot/src/main/java/org/camunda/bpm/sendMessages/Customer/InvalidInformationMessageDelegate.java
+++ emtours-spring-boot/src/main/java/org/camunda/bpm/sendMessages/Customer/InvalidInformationMessageDelegate.java
@@ -1,13 +1,53 @@
 package org.camunda.bpm.sendMessages.Customer;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+@RestController
 public class InvalidInformationMessageDelegate implements JavaDelegate {
 
 	public void execute(DelegateExecution execution) throws Exception {
-
-	// TODO Auto-generated method stub
+		String test = doPost("Testing!!!!!!!");
+		System.out.println("result: "+test);
 	
 	}
+	
+	  
+	private String doPost(String string) {
+		HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+	      
+	      //System.out.println(req_payload.toString());
+	      //System.out.println(headers.toString());
+
+	      HttpEntity<String> request = new HttpEntity<>(string, headers);
+	      UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/testSend");
+	      builder.queryParam("name", string);
+	     
+	      //String url = "http://localhost:8080/testSend";
+
+	      String result = "";
+	      ResponseEntity<?> response = new RestTemplate().postForEntity(builder.build().encode().toUri(), request, String.class);
+	      HttpStatus statusCode = response.getStatusCode();
+	      System.out.println(statusCode.toString());
+	      if (statusCode == HttpStatus.ACCEPTED) {
+	          result = (String) response.getBody();
+	      }
+	      return result;
+		}
 }
