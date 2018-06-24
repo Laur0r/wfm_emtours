7859236ddec334b4619a2ae836f93e2dc4587242
cfu01 <c.fukas@gmx.de>
2018-06-24 22:37:11 +0200

    Communication with Funspark

---
+++ emtours-spring-boot/src/main/java/org/camunda/bpm/properties/Funspark.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.properties;
+
+public class Funspark {
+	private String location;
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
