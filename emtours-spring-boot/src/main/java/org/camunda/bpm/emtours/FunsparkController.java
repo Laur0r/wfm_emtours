b3ad59baed2217de87c193149fe10bd0cf0c0459
Jule1 <juliane.lange1389@gmail.com>
2018-07-03 11:16:30 +0200

    receive FunSparkMessages

--- emtours-spring-boot/src/main/java/org/camunda/bpm/emtours/FunsparkController.java
+++ emtours-spring-boot/src/main/java/org/camunda/bpm/emtours/FunsparkController.java
@@ -1,8 +1,14 @@
 package org.camunda.bpm.emtours;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Optional;
+
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.camunda.bpm.entities.Activity;
+import org.camunda.bpm.entities.Recommendation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class FunsparkController implements ExecutionListener{
 	
 	@Autowired
 	private ProcessEngine camunda;
 	private String executionId;
+	
+	@Autowired(required = true)
+	public ActivityRepository activityRepository;
+	
+	@Autowired(required = true)
+	public RecommendationRepository recoRepository;
 	
 	@RequestMapping(value="/testSend", method=RequestMethod.POST)
 	public ResponseEntity<String> testPOST(@RequestBody String cust) {
@@ -39,6 +52,44 @@ public class FunsparkController implements ExecutionListener{
 		
 		return "";
 	}
+	
+	
+	@RequestMapping(value="/activityRecommendations", method=RequestMethod.POST)
+	public String receiveActivityRecos(Collection<Activity> activityRecos) {
+		System.out.println("received Activity Recommendations: "+ activityRecos);
+		for (Iterator<Activity> iterator = activityRecos.iterator(); iterator.hasNext();) {
+			Activity a = iterator.next();
+			a = activityRepository.save(a);
+		}
+		camunda.getRuntimeService().messageEventReceived("activityRecos", executionId);
+		
+		return "";
+	}
+	
+	@RequestMapping(value="/bookingUnavailable", method=RequestMethod.POST)
+	public String receiveUnavailability(String unavailable) {
+		System.out.println("received unavailibility notification");
+		
+		camunda.getRuntimeService().messageEventReceived("unavailable", executionId);
+		
+		return "";
+	}
+	
+	@RequestMapping(value="/booking&bill", method=RequestMethod.POST)
+	public String receiveBookingAndBill(Collection<Activity> activityBooking, double costs) {
+		System.out.println("received booking confirmation and bill from FunSpark");
+		
+		int recommendationId = (Integer)camunda.getRuntimeService().getVariable(executionId, "recommendationId");
+		Optional<Recommendation> recommendationo = recoRepository.findById(recommendationId);
+		Recommendation recommendation = recommendationo.get();
+		recommendation.setActivityCost(costs);
+		recommendation = recoRepository.save(recommendation);
+		
+		camunda.getRuntimeService().messageEventReceived("bookingandbill", executionId);
+		
+		return "";
+	}
+	
 
 	@Override
 	public void notify(DelegateExecution execution) throws Exception {
