972f7c3f5cfae32f7ca2edb4dce491c40257c88e
Laura Troost <l_troo01@uni-muenster.de>
2018-07-07 13:57:24 +0200

    Add update activity to model + activity email fix

--- emtours-spring-boot/src/main/java/org/camunda/bpm/services/EnhanceRecommendationDelegate.java
+++ emtours-spring-boot/src/main/java/org/camunda/bpm/services/EnhanceRecommendationDelegate.java
@@ -1,20 +1,13 @@
 package org.camunda.bpm.services;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.camunda.bpm.emtours.ActivityRepository;
 import org.camunda.bpm.emtours.RecommendationRepository;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.camunda.bpm.entities.Activity;
-import org.camunda.bpm.entities.Recommendation;
-import org.camunda.bpm.sendMessages.Funspark.ActivityDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @Component
 public class EnhanceRecommendationDelegate implements JavaDelegate {
@@ -28,23 +21,6 @@ public class EnhanceRecommendationDelegate implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 			
-		String activityJson = (String) execution.getVariable("activityBooking");
-		ObjectMapper mapper = new ObjectMapper();
-		List<ActivityDate> activityBooking = mapper.readValue(activityJson, new TypeReference<List<ActivityDate>>(){});
-			
-		int recommendationId = (Integer) execution.getVariable("recommendationId");
-		Optional<Recommendation> reco = recorepository.findById(recommendationId);
-		Recommendation recommendation = reco.get();
-		List<Activity> oldrecos = (List<Activity>)recommendation.getActivities();
-		
-		for(ActivityDate booked: activityBooking) {
-			for(Activity old: oldrecos) {
-				if (booked.getActivity().getName() == old.getName()) {
-					old.setDate(booked.getBookingDate());
-					actrepository.save(old);
-				}
-			}
-		}	
 	}
 }
 
