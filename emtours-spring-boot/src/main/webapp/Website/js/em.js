a32cdc36ecbc8f062c4bdf971bdf63c38ec372a0
Julius Klein <julius.klein@uni-muenster.de>
2018-07-09 09:26:49 +0200

    Initial Customer Input validation

--- emtours-spring-boot/src/main/webapp/Website/js/em.js
+++ emtours-spring-boot/src/main/webapp/Website/js/em.js
@@ -22,14 +22,39 @@ $(document).ready(function(){
     var first_name = document.getElementById('first_name');
     var last_name = document.getElementById('last_name');
 		var email = document.getElementById('email');
+    var gender = document.getElementById('gender');
+    var adress = document.getElementById('adress');
+    var zip_code = document.getElementById('zip_code');
+    var city = document.getElementById('city');
+    var country = document.getElementById('country');
+    var birthday = document.getElementById('birthday');
+    var departure_date = document.getElementById('departure_date');
+    var return_date = document.getElementById('return_date');
+    var budget = document.getElementById('budget');
+    var climate = document.getElementById('climate');
+    var num_adults = document.getElementById('num_adults');
+    var num_activities = document.getElementById('num_activities');
+    var activity = document.getElementById('activity');
     if (first_name.value != null && first_name.value != undefined && first_name.value != "" && first_name.validity.valid != false
     && last_name.value != undefined && last_name.value != null && last_name.value != "" && last_name.validity.valid != false
-		&& email.value != undefined && email.value != null && email.value != "" && email.validity.valid != false) {
+		&& email.value != undefined && email.value != null && email.value != "" && email.validity.valid != false)
+    && gender.value != undefined && gender.value != null && gender.value != "" && gender.validity.valid != false
+    && adress.value != undefined && adress.value != null && adress.value != "" && adress.validity.valid != false
+    && zip_code.value != undefined && zip_code.value != null && zip_code.value != "" && zip_code.validity.valid != false
+    && city.value != undefined && city.value != null && city.value != "" && city.validity.valid != false
+    && country.value != undefined && country.value != null && country.value != "" && country.validity.valid != false
+    && birthday.value != undefined && birthday.value != null && birthday.value != "" && birthday.validity.valid != false
+    && departure_date.value != undefined && departure_date.value != null && departure_date.value != "" && departure_date.validity.valid != false
+    && return_date.value != undefined && return_date.value != null && return_date.value != "" && return_date.validity.valid != false && return_date.value > departure_date.value
+    && budget.value != undefined && budget.value != null && budget.value != "" && budget.validity.valid != false
+    && climate.value != undefined && climate.value != null && climate.value != "" && climate.validity.valid != false
+    && num_adults.value != undefined && num_adults.value != null && num_adults.value != "" && num_adults.validity.valid != false
+     {
 			submit();
 		} else {
       console.log("Empty or invalid input fields")
       document.getElementById("errormessage").innerHTML = "Please fill out every fields with valid values.";
-      $('#fail').modal('open');   
+      $('#fail').modal('open');
 		}
 	});
 });
@@ -68,7 +93,7 @@ function submit() {
     processData: false,
     success: function(data) {
       console.log("success");
-      $('#success').modal('open');     
+      $('#success').modal('open');
       console.log(data);
     },
     error: function (jqxhr) {
@@ -77,13 +102,13 @@ function submit() {
       } else {
         document.getElementById("errormessage").innerHTML = "Please try again in a few seconds.";
       }
-      $('#fail').modal('open');   
+      $('#fail').modal('open');
       if (jqxhr !== undefined) {
         console.log(jqxhr);
       }
     }
   })
-	
+
 };
 
 // Function for sending Response for Recommendations
@@ -95,7 +120,7 @@ function sendResponse() {
   if (feedback != undefined && feedback != null && feedback.value != "") {
       var customer_information = {
           "executionId": executionId,
-          "customerResponse": feedback.value 
+          "customerResponse": feedback.value
       }
       var json_customer_information = JSON.stringify(customer_information);
       console.log("Post Request:" + json_customer_information);
@@ -111,7 +136,7 @@ function sendResponse() {
           processData: false,
           success: function(data) {
               console.log("success");
-              $('#success').modal('open');     
+              $('#success').modal('open');
               console.log(data);
           },
           error: function (jqxhr) {
@@ -120,7 +145,7 @@ function sendResponse() {
               } else {
                   document.getElementById("errormessage").innerHTML = "Please try again in a few seconds.";
               }
-              $('#fail').modal('open');   
+              $('#fail').modal('open');
               if (jqxhr !== undefined) {
                   console.log(jqxhr);
               }
@@ -129,7 +154,6 @@ function sendResponse() {
   } else {
       console.log("Empty or invalid input fields");
       document.getElementById("errormessage").innerHTML = "Please fill select your response.";
-      $('#fail').modal('open'); 
+      $('#fail').modal('open');
   }
 };
-
